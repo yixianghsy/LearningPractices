090dd40da94cf43e3fad2ec278dc921ec55b1ec9
yixianghsy <yixianghsy@gmail.com>
2022-09-24 23:09:17 +0800

    第九章日志讲解和Logback配置实战

--- java/base_project/src/main/java/net/xdclass/base_project/XdclassApplication.java
+++ java/base_project/src/main/java/net/xdclass/base_project/XdclassApplication.java
@@ -2,12 +2,8 @@ package net.xdclass.base_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication //一个注解顶下面3个
-@EnableScheduling	//开启定时任务
-@EnableAsync   //开启异步任务
 public class XdclassApplication {
 
 	public static void main(String[] args) {
--- java/base_project/src/main/java/net/xdclass/base_project/controller/UserController.java
+++ java/base_project/src/main/java/net/xdclass/base_project/controller/UserController.java
@@ -1,11 +1,9 @@
 package net.xdclass.base_project.controller;
 
-import java.util.concurrent.Future;
-
 import net.xdclass.base_project.domain.JsonData;
-import net.xdclass.base_project.task.AsyncTask;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,34 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 	
-	@Autowired
-	private AsyncTask task;
 	
-	@GetMapping("async_task")
-	public JsonData exeTask() throws InterruptedException{
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	
+	@GetMapping("log")
+	public Object testLog(){
 		
-		long begin = System.currentTimeMillis();
-		
-//		task.task1();
-//		task.task2();
-//		task.task3();
-
-		Future<String> task4 = task.task4();
-		Future<String> task5 = task.task5();
-		Future<String> task6 = task.task6();
-		for(;;){
-			if (task4.isDone() && task5.isDone() && task6.isDone()) {
-				break;
-			}
-		}
-		
-		
-		long end = System.currentTimeMillis();
-		
-		long total = end-begin;
-		System.out.println("执行总耗时="+total);
-		return JsonData.buildSuccess(total);
+		logger.debug("this is debug level");
+		logger.info("this is info level ");
+		logger.warn("this is warn level ");
+		logger.error("this is error level");
+		return JsonData.buildSuccess();
 	}
-	
 	
 }
