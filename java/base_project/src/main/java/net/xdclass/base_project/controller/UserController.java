3fd7a1ff8deaf73e87cae14f7a2faafaa2d718bd
yixianghsy <yixianghsy@gmail.com>
2022-09-24 23:04:22 +0800

    第九章常用定时任务配置实战

--- java/base_project/src/main/java/net/xdclass/base_project/controller/UserController.java
+++ java/base_project/src/main/java/net/xdclass/base_project/controller/UserController.java
@@ -1,7 +1,11 @@
 package net.xdclass.base_project.controller;
 
-import net.xdclass.base_project.domain.JsonData;
+import java.util.concurrent.Future;
 
+import net.xdclass.base_project.domain.JsonData;
+import net.xdclass.base_project.task.AsyncTask;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +15,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class UserController {
 
-
-	@GetMapping(value="user")
-	public Object account(){
-		
-		return JsonData.buildSuccess("www.xdclass.net");
-		
-	}
 	
+	@Autowired
+	private AsyncTask task;
+	
+	@GetMapping("async_task")
+	public JsonData exeTask() throws InterruptedException{
+		
+		long begin = System.currentTimeMillis();
+		
+//		task.task1();
+//		task.task2();
+//		task.task3();
+
+		Future<String> task4 = task.task4();
+		Future<String> task5 = task.task5();
+		Future<String> task6 = task.task6();
+		for(;;){
+			if (task4.isDone() && task5.isDone() && task6.isDone()) {
+				break;
+			}
+		}
+		
+		
+		long end = System.currentTimeMillis();
+		
+		long total = end-begin;
+		System.out.println("执行总耗时="+total);
+		return JsonData.buildSuccess(total);
+	}
 	
 	
 }
