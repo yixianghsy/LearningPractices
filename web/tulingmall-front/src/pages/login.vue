57d7080a754386d90b35fdd08c8e97bd1ca28cdc
yixianghsy <yixianghsy@gmail.com>
2022-12-14 00:02:11 +0800

    登录账户名修复

--- web/tulingmall-front/src/pages/login.vue
+++ web/tulingmall-front/src/pages/login.vue
@@ -65,17 +65,17 @@ export default {
          }),{headers: {'Content-Type': 'application/x-www-form-urlencoded'}}).then((res)=>{
        
            // 将 tokenHead+jwtToken 存入到cookie  下次请求就可以携带在请求头中了
-          // Todo:后续jwt改造 this.$cookie.set('token',res.tokenHead+' '+res.token,{expires:'1M'});
+          this.$cookie.set('token',res.tokenHead+' '+res.token,{expires:'1M'});
           // 保存到全局变量中
-          // this.$store.dispatch('saveToken',res.token);
+          this.$store.dispatch('saveToken',res.token);
           // 拿到payloader 解码
-          // Todo:后续jwt改造 var tokenStr= decodeURIComponent(escape(window.atob(res.token.split('.')[1])));
+          var tokenStr= decodeURIComponent(escape(window.atob(res.token.split('.')[1])));
           // 转换为json对象
-          // Todo:后续jwt改造 let username = JSON.parse(tokenStr).user_name;
+          let username = JSON.parse(tokenStr).user_name;
 
-          let username=res.username
+          // let username=res.username
           
-           setCookie("token",username,120);
+          // setCookie("token",username,120);
 
           // setCookie=this.$cookie.set
            setCookie("username",username,120);
