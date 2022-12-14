52b467fdafd77afebd3ece0d519e59cd2470710e
yixianghsy <yixianghsy@gmail.com>
2022-12-14 15:24:15 +0800

    test

---
+++ web/jingdong/dist/js/chunk-2d0bd977.e5bb5b02.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0bd977"],{"2d3b":function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("\n    搜索页\n")])},u=[],a={},r=a,s=t("2877"),d=Object(s["a"])(r,c,u,!1,null,"61abc893",null);e["default"]=d.exports}}]);
+//# sourceMappingURL=chunk-2d0bd977.e5bb5b02.js.map
