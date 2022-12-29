0e8486a84f710ae80492c5e98d38a1ccef90dfc2
yixianghsy <yixianghsy@gmail.com>
2022-12-29 09:33:10 +0800

    分⽚策略实战之Hint分⽚算法

--- java/sharding-jdbc/src/test/java/net/xdclass/db/DbTest.java
+++ java/sharding-jdbc/src/test/java/net/xdclass/db/DbTest.java
@@ -7,6 +7,7 @@ import net.xdclass.mapper.AdConfigMapper;
 import net.xdclass.mapper.ProductOrderMapper;
 import net.xdclass.model.AdConfigDO;
 import net.xdclass.model.ProductOrderDO;
+import org.apache.shardingsphere.api.hint.HintManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -131,4 +132,29 @@ public class DbTest {
         productOrderMapper.selectList(new QueryWrapper<ProductOrderDO>().eq("id",66L).eq("user_id",99L));
     }
 
+    /**
+     * 正常可以用AOP进行实现
+     */
+    @Test
+    public void testHit(){
+
+        //清除历史规则
+        HintManager.clear();
+
+        //获取对应的实例
+        HintManager hintManager = HintManager.getInstance();
+
+        //设置库的分片键值，value是用于库分片取模
+        hintManager.addDatabaseShardingValue("product_order",3L);
+
+        //设置表的分片键值，value是用于表分片取模
+        hintManager.addTableShardingValue("product_order",8L);
+
+        //如果在读写分离数据库中，Hint 可以强制读主库（主从复制存在一定延时，但在业务场景中，可能更需要保证数据的实时性）
+        //hintManager.setMasterRouteOnly();
+
+        //对应的value,只做查询，不做sql解析
+        productOrderMapper.selectList(new QueryWrapper<ProductOrderDO>().eq("id",66L));
+
+    }
 }
