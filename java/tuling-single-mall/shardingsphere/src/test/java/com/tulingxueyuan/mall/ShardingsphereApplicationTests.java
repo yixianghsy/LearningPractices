8fc4fbb5d5df5ab3f2854605af81a494f5937f96
yixianghsy <yixianghsy@gmail.com>
2023-01-06 10:36:44 +0800

    shardingsphere读写分离

--- java/tuling-single-mall/shardingsphere/src/test/java/com/tulingxueyuan/mall/ShardingsphereApplicationTests.java
+++ java/tuling-single-mall/shardingsphere/src/test/java/com/tulingxueyuan/mall/ShardingsphereApplicationTests.java
@@ -1,13 +1,34 @@
 package com.tulingxueyuan.mall;
 
+import com.tulingxueyuan.mall.modules.ums.model.UmsMenu;
+import com.tulingxueyuan.mall.modules.ums.model.UmsRole;
+import com.tulingxueyuan.mall.modules.ums.service.UmsAdminService;
+import com.tulingxueyuan.mall.modules.ums.service.UmsRoleService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ShardingsphereApplicationTests {
+	@Autowired
+	private UmsAdminService adminService;
+	@Autowired
+	UmsRoleService umsRoleService;
 
 	@Test
 	void contextLoads() {
+		List<UmsRole> umsRoles = adminService.getRoleList(8L);
+		umsRoles.forEach((srt)->{
+			System.out.println(srt);
+		});
 	}
-
+	@Test
+	void  umsRoleService(){
+		List<UmsMenu> list = umsRoleService.getMenuList(8L);
+		list.forEach((srt)->{
+			System.out.println(srt);
+		});
+	}
 }
