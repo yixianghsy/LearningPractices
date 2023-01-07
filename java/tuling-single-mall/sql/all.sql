048e031d656c3a8f8efe62b5626beee14a04042c
yixianghsy <yixianghsy@gmail.com>
2023-01-07 18:08:01 +0800

    首页数据库主从配置

--- java/tuling-single-mall/sql/all.sql
+++ java/tuling-single-mall/sql/all.sql
@@ -422,6 +422,49 @@ INSERT INTO `oms_order` VALUES ('24', '1', '2', '201809130101000005', '2018-09-1
 INSERT INTO `oms_order` VALUES ('25', '1', '2', '201809130102000006', '2018-09-13 17:03:00', 'test', '18732.00', '16377.75', '10.00', '2344.25', '0.00', '10.00', '5.00', '1', '1', '4', '0', null, null, '15', '18682', '18682', '单品促销,打折优惠：满3件，打7.50折,满减优惠：满1000.00元，减120.00元,满减优惠：满1000.00元，减120.00元,无优惠', null, null, null, null, null, '大梨22', '18033441849', '518000', '北京市', '北京城区', '东城区', '东城街道', 'xxx', '0', '0', null, null, null, null, null, '2018-10-30 15:08:31');
 INSERT INTO `oms_order` VALUES ('26', '1', '2', '201809140101000003', '2018-09-14 16:16:16', 'test', '18732.00', '16377.75', '0.00', '2344.25', '0.00', '10.00', '0.00', '0', '1', '4', '0', null, null, '15', '18682', '18682', '单品促销,打折优惠：满3件，打7.50折,满减优惠：满1000.00元，减120.00元,满减优惠：满1000.00元，减120.00元,无优惠', null, null, null, null, null, '大梨', '18033441849', '518000', '广东省', '深圳市', '福田区', '东晓街道', null, '0', '0', null, null, null, null, null, null);
 
+
+
+-- ----------------------------
+-- Table structure for ums_member
+-- ----------------------------
+DROP TABLE IF EXISTS `ums_member`;
+CREATE TABLE `ums_member` (
+                              `id` bigint(20) NOT NULL AUTO_INCREMENT,
+                              `member_level_id` bigint(20) DEFAULT NULL,
+                              `username` varchar(64) DEFAULT NULL COMMENT '用户名',
+                              `password` varchar(64) DEFAULT NULL COMMENT '密码',
+                              `nickname` varchar(64) DEFAULT NULL COMMENT '昵称',
+                              `phone` varchar(64) DEFAULT NULL COMMENT '手机号码',
+                              `status` int(1) DEFAULT NULL COMMENT '帐号启用状态:0->禁用；1->启用',
+                              `create_time` datetime DEFAULT NULL COMMENT '注册时间',
+                              `icon` varchar(500) DEFAULT NULL COMMENT '头像',
+                              `gender` int(1) DEFAULT NULL COMMENT '性别：0->未知；1->男；2->女',
+                              `birthday` date DEFAULT NULL COMMENT '生日',
+                              `city` varchar(64) DEFAULT NULL COMMENT '所做城市',
+                              `job` varchar(100) DEFAULT NULL COMMENT '职业',
+                              `personalized_signature` varchar(200) DEFAULT NULL COMMENT '个性签名',
+                              `source_type` int(1) DEFAULT NULL COMMENT '用户来源',
+                              `integration` int(11) DEFAULT NULL COMMENT '积分',
+                              `growth` int(11) DEFAULT NULL COMMENT '成长值',
+                              `luckey_count` int(11) DEFAULT NULL COMMENT '剩余抽奖次数',
+                              `history_integration` int(11) DEFAULT NULL COMMENT '历史积分数量',
+                              PRIMARY KEY (`id`),
+                              UNIQUE KEY `idx_username` (`username`),
+                              UNIQUE KEY `idx_phone` (`phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='会员表';
+
+-- ----------------------------
+-- Records of ums_member
+-- ----------------------------
+INSERT INTO `ums_member` VALUES ('1', '4', 'test', '$2a$10$zUnskPAYscI1P4qQYICN.OvFU63eELVwqegx/thOqkxN2shB5KDEy', 'windir', '18061581849', '1', '2018-08-02 10:35:44', null, '1', '2009-06-01', '上海', '学生', 'test', null, '5000', null, null, null);
+INSERT INTO `ums_member` VALUES ('3', '4', 'windy', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', 'windy', '18061581848', '1', '2018-08-03 16:46:38', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `ums_member` VALUES ('4', '4', 'zhengsan', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', 'zhengsan', '18061581847', '1', '2018-11-12 14:12:04', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `ums_member` VALUES ('5', '4', 'lisi', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', 'lisi', '18061581841', '1', '2018-11-12 14:12:38', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `ums_member` VALUES ('6', '4', 'wangwu', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', 'wangwu', '18061581842', '1', '2018-11-12 14:13:09', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `ums_member` VALUES ('7', '4', 'lion', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', 'lion', '18061581845', '1', '2018-11-12 14:21:39', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `ums_member` VALUES ('8', '4', 'shari', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', 'shari', '18061581844', '1', '2018-11-12 14:22:00', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `ums_member` VALUES ('9', '4', 'aewen', '$2a$10$NZ5o7r2E.ayT2ZoxgjlI.eJ6OEYqjH7INR/F.mXDbjZJi9HF0YCVG', 'aewen', '18061581843', '1', '2018-11-12 14:22:55', null, null, null, null, null, null, null, null, null, null, null);
+
 -- ----------------------------
 -- Table structure for oms_order_item
 -- ----------------------------
