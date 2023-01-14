dd33e64ba29244498c137ee218948fbcdb71bf01
yixianghsy@gmail.com <yixianghsy@gmail.com>
2023-01-14 13:00:47 +0800

    新增es配置

---
+++ java/tuling-single-mall/tuling-common/src/main/java/com/tulingxueyuan/mall/common/util/ResultUtil.java
@@ -0,0 +1,46 @@
+package com.tulingxueyuan.mall.common.util;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class ResultUtil implements Serializable {
+
+    private String code;
+
+    private String msg;
+
+    private Upload data;
+
+    private  int status;
+
+    public static ResultUtil success(Upload data) {
+        return resultData(CodeEnum.SUCCESS.val(), CodeEnum.SUCCESS.msg(), data);
+    }
+
+    public static ResultUtil success(Upload data, String msg) {
+        return resultData(CodeEnum.SUCCESS.val(), msg, data);
+    }
+
+    public static ResultUtil fail(String code, String msg) {
+        return resultData(code, msg, null);
+    }
+
+    public static ResultUtil fail(String code, String msg, Upload data) {
+        return resultData(code, msg, data);
+    }
+
+    private static ResultUtil resultData(String code, String msg, Upload data) {
+        ResultUtil resultData = new ResultUtil();
+        resultData.setCode(code);
+        resultData.setMsg(msg);
+        resultData.setData(data);
+        return resultData;
+    }
+
+
+    public void setstatus(int i) {
+    }
+}
+
