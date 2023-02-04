5ce474c75bdcba9d163efb5933febc481335bffc
yixianghsy <yixianghsy@gmail.com>
2023-02-04 11:58:18 +0800

    新增dev和pord配置

--- web/tuling_mall_web_admin/config/dev.env.js
+++ web/tuling_mall_web_admin/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://192.168.1.27:8099"' 
+  BASE_API: '"http://127.0.0.1:8099"' 
 })
