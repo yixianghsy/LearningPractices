e62529be56bea1d73c06dfabde5bb10b30017ef5
yixianghsy <yixianghsy@gmail.com>
2023-02-14 08:43:34 +0800

    删除多余模块

--- java/book-small-spring/spring-step-15/src/main/java/cn/bugstack/springframework/beans/factory/HierarchicalBeanFactory.java
+++
@@ -1,9 +0,0 @@
-package cn.bugstack.springframework.beans.factory;
-
-/**
- * Sub-interface implemented by bean factories that can be part
- * of a hierarchy.
- */
-public interface HierarchicalBeanFactory extends BeanFactory {
-
-}
--- java/book-small-spring/spring-step-20/src/test/java/cn/bugstack/middleware/mybatis/test/dao/IUserDao.java
+++
@@ -1,9 +0,0 @@
-package cn.bugstack.middleware.mybatis.test.dao;
-
-import cn.bugstack.middleware.mybatis.test.po.User;
-
-public interface IUserDao {
-
-     User queryUserInfoById(Long id);
-
-}
