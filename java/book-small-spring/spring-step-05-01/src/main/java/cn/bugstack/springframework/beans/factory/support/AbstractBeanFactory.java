54cf356c482e84a02332ec5fa279990b67814d7a
yixianghsy <yixianghsy@gmail.com>
2023-02-18 00:44:27 +0800

    第五章

---
+++ java/book-small-spring/spring-step-05-01/src/main/java/cn/bugstack/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -0,0 +1,48 @@
+package cn.bugstack.springframework.beans.factory.support;
+
+import cn.bugstack.springframework.beans.BeansException;
+import cn.bugstack.springframework.beans.factory.BeanFactory;
+import cn.bugstack.springframework.beans.factory.config.BeanDefinition;
+
+/**
+ *
+ *
+ *
+ * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
+ * @description 抽象的 Bean 工厂基类，定义模板方法
+ * @date 2022/03/07
+ *
+ *
+ */
+public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements BeanFactory {
+
+    @Override
+    public Object getBean(String name) throws BeansException {
+        return doGetBean(name, null);
+    }
+
+    @Override
+    public Object getBean(String name, Object... args) throws BeansException {
+        return doGetBean(name, args);
+    }
+
+    @Override
+    public <T> T getBean(String name, Class<T> requiredType) throws BeansException {
+        return (T) getBean(name);
+    }
+
+    protected <T> T doGetBean(final String name, final Object[] args) {
+        Object bean = getSingleton(name);
+        if (bean != null) {
+            return (T) bean;
+        }
+
+        BeanDefinition beanDefinition = getBeanDefinition(name);
+        return (T) createBean(name, beanDefinition, args);
+    }
+
+    protected abstract BeanDefinition getBeanDefinition(String beanName);
+
+    protected abstract Object createBean(String beanName, BeanDefinition beanDefinition, Object[] args);
+
+}
