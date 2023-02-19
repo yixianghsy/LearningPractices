b8256103e8b044d13eb3a3b36878a9610ede1053
yixianghsy <yixianghsy@gmail.com>
2023-02-19 22:09:04 +0800

    spring 06学习

---
+++ java/book-small-spring/spring-step-06-01/src/main/java/cn/bugstack/springframework/beans/factory/config/BeanReference.java
@@ -0,0 +1,25 @@
+package cn.bugstack.springframework.beans.factory.config;
+
+/**
+ *
+ *
+ *
+ * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
+ * @description Bean 引用
+ * @date 2022/3/9
+ *  /CodeDesignTutorials
+ *
+ */
+public class BeanReference {
+
+    private final String beanName;
+
+    public BeanReference(String beanName) {
+        this.beanName = beanName;
+    }
+
+    public String getBeanName() {
+        return beanName;
+    }
+
+}
