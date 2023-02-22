30c891fed0244f49c39774fad608c3e02efd3ecb
yixianghsy <yixianghsy@gmail.com>
2023-02-22 10:01:00 +0800

    spring08学习

---
+++ java/book-small-spring/spring-step-08-01/src/test/java/cn/bugstack/springframework/test/ApiTest.java
@@ -0,0 +1,34 @@
+package cn.bugstack.springframework.test;
+
+import cn.bugstack.springframework.context.support.ClassPathXmlApplicationContext;
+import cn.bugstack.springframework.test.bean.UserService;
+import org.junit.Test;
+
+/**
+ *
+ *
+ *
+ * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
+ * @description 单元测试
+ * @date 2022/03/10
+ *
+ *
+ */
+public class ApiTest {
+
+    @Test
+    public void test_xml() {
+        // 1.初始化 BeanFactory
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring.xml");
+        applicationContext.registerShutdownHook();
+
+        // 2. 获取Bean对象调用方法
+        UserService userService = applicationContext.getBean("userService", UserService.class);
+        String result = userService.queryUserInfo();
+        System.out.println("测试结果：" + result);
+
+        System.out.println("ApplicationContextAware："+userService.getApplicationContext());
+        System.out.println("BeanFactoryAware："+userService.getBeanFactory());
+    }
+
+}
