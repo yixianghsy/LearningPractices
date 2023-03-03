977c1b99485adf039f2e2cd8f64bf28dbce16d8d
yixianghsy <yixianghsy@gmail.com>
2023-03-03 10:09:00 +0800

    10 未完成

---
+++ java/book-small-spring/spring-step-10-01/src/main/java/cn/bugstack/springframework/context/event/ContextClosedEvent.java
@@ -0,0 +1,24 @@
+package cn.bugstack.springframework.context.event;
+/**
+ *
+ *
+ *
+ * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
+ * @description Event raised when an <code>ApplicationContext</code> gets closed.
+ * @date 2022/3/13
+ *  /CodeDesignTutorials
+ *
+ */
+public class ContextClosedEvent  extends ApplicationContextEvent{
+
+    /**
+     * Constructs a prototypical Event.
+     *
+     * @param source The object on which the Event initially occurred.
+     * @throws IllegalArgumentException if source is null.
+     */
+    public ContextClosedEvent(Object source) {
+        super(source);
+    }
+
+}
---
+++ java/book-small-spring/spring-step-10-01/src/main/java/cn/bugstack/springframework/context/event/ContextRefreshedEvent.java
@@ -0,0 +1,22 @@
+package cn.bugstack.springframework.context.event;
+/**
+ *
+ *
+ *
+ * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
+ * @description Event raised when an <code>ApplicationContext</code> gets initialized or refreshed.
+ * @date 2022/3/13
+ *  /CodeDesignTutorials
+ *
+ */
+public class ContextRefreshedEvent extends ApplicationContextEvent{
+    /**
+     * Constructs a prototypical Event.
+     *
+     * @param source The object on which the Event initially occurred.
+     * @throws IllegalArgumentException if source is null.
+     */
+    public ContextRefreshedEvent(Object source) {
+        super(source);
+    }
+}
