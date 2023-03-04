81d0f12541e6e987cd815a8d87a5494dc9975311
yixianghsy <yixianghsy@gmail.com>
2023-03-04 13:47:41 +0800

    13-01 init

---
+++ java/book-small-spring/spring-step-13-01/src/main/java/cn/bugstack/springframework/context/event/SimpleApplicationEventMulticaster.java
@@ -0,0 +1,31 @@
+package cn.bugstack.springframework.context.event;
+
+import cn.bugstack.springframework.beans.factory.BeanFactory;
+import cn.bugstack.springframework.context.ApplicationEvent;
+import cn.bugstack.springframework.context.ApplicationListener;
+
+/**
+ *
+ *
+ *
+ * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
+ * @description Simple implementation of the {@link ApplicationEventMulticaster} interface.
+ * @date 2022/3/13
+ *  /CodeDesignTutorials
+ *
+ */
+public class SimpleApplicationEventMulticaster extends AbstractApplicationEventMulticaster {
+
+    public SimpleApplicationEventMulticaster(BeanFactory beanFactory) {
+        setBeanFactory(beanFactory);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void multicastEvent(final ApplicationEvent event) {
+        for (final ApplicationListener listener : getApplicationListeners(event)) {
+            listener.onApplicationEvent(event);
+        }
+    }
+
+}
