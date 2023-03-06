7da3edc4441106e8a6d8aa683492c8ec3537e7ba
yixianghsy <yixianghsy@gmail.com>
2023-03-06 11:37:56 +0800

    17-01 init

---
+++ java/book-small-spring/spring-step-17-01/src/main/java/cn/bugstack/springframework/context/support/AbstractRefreshableApplicationContext.java
@@ -0,0 +1,43 @@
+package cn.bugstack.springframework.context.support;
+
+import cn.bugstack.springframework.beans.BeansException;
+import cn.bugstack.springframework.beans.factory.ConfigurableListableBeanFactory;
+import cn.bugstack.springframework.beans.factory.support.DefaultListableBeanFactory;
+
+/**
+ *
+ *
+ *
+ * 作者：DerekYRC https://github.com/DerekYRC/mini-spring
+ * @description 抽象基类刷新应用上下文 Base class for {@link cn.bugstack.springframework.context.ApplicationContext}
+ * implementations which are supposed to support multiple calls to {@link #refresh()},
+ * creating a new internal bean factory instance every time.
+ * Typically (but not necessarily), such a context will be driven by
+ * a set of config locations to load bean definitions from.
+ * @date 2022/3/10
+ *
+ *
+ */
+public abstract class AbstractRefreshableApplicationContext extends AbstractApplicationContext {
+
+    private DefaultListableBeanFactory beanFactory;
+
+    @Override
+    protected void refreshBeanFactory() throws BeansException {
+        DefaultListableBeanFactory beanFactory = createBeanFactory();
+        loadBeanDefinitions(beanFactory);
+        this.beanFactory = beanFactory;
+    }
+
+    private DefaultListableBeanFactory createBeanFactory() {
+        return new DefaultListableBeanFactory();
+    }
+
+    protected abstract void loadBeanDefinitions(DefaultListableBeanFactory beanFactory);
+
+    @Override
+    protected ConfigurableListableBeanFactory getBeanFactory() {
+        return beanFactory;
+    }
+
+}
