fd42d7900be5b84249c9cbc274640bddec3cccd3
yixianghsy <yixianghsy@gmail.com>
2023-03-07 09:58:09 +0800

    18-01 init

---
+++ java/book-small-spring/spring-step-18-01/src/main/java/cn/bugstack/springframework/context/support/ApplicationContextAwareProcessor.java
@@ -0,0 +1,36 @@
+package cn.bugstack.springframework.context.support;
+
+import cn.bugstack.springframework.beans.BeansException;
+import cn.bugstack.springframework.beans.factory.config.BeanPostProcessor;
+import cn.bugstack.springframework.context.ApplicationContext;
+import cn.bugstack.springframework.context.ApplicationContextAware;
+
+/**
+ *
+ * @description 通过 BeanPostProcessor 实现类感知应用上下文对象
+ * @date 2022/3/11
+ *  /CodeDesignTutorials
+ *
+ */
+public class ApplicationContextAwareProcessor implements BeanPostProcessor {
+
+    private final ApplicationContext applicationContext;
+
+    public ApplicationContextAwareProcessor(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof ApplicationContextAware){
+            ((ApplicationContextAware) bean).setApplicationContext(applicationContext);
+        }
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+}
---
+++ java/book-small-spring/spring-step-18-01/src/main/java/cn/bugstack/springframework/core/convert/converter/ConverterFactory.java
@@ -0,0 +1,20 @@
+package cn.bugstack.springframework.core.convert.converter;
+
+/**
+ *
+ * @description 类型转换工厂
+ * @date 2022/3/16
+ *
+ *
+ */
+public interface ConverterFactory<S, R>{
+
+    /**
+     * Get the converter to convert from S to target type T, where T is also an instance of R.
+     * @param <T> the target type
+     * @param targetType the target type to convert to
+     * @return a converter from S to T
+     */
+    <T extends R> Converter<S, T> getConverter(Class<T> targetType);
+
+}
