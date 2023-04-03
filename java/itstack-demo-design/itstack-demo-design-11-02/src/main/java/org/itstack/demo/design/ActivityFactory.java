884c34d46c61bec29ef4d55c285dbd0338a4850c
yixianghsy <yixianghsy@gmail.com>
2023-04-03 14:17:19 +0800

    享元设计模式学习

--- java/itstack-demo-design/itstack-demo-design-11-02/src/main/java/org/itstack/demo/design/ActivityFactory.java
+++ java/itstack-demo-design/itstack-demo-design-11-02/src/main/java/org/itstack/demo/design/ActivityFactory.java
@@ -8,6 +8,7 @@ import java.util.Map;
  * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
  * 公众号：bugstack虫洞栈
  * Create by 小傅哥(fustack) @2020
+ * 享元⼯⼚
  */
 public class ActivityFactory {
 
--- java/itstack-demo-design/itstack-demo-design-11-02/src/test/java/org/itstack/demo/design/test/ApiTest.java
+++ java/itstack-demo-design/itstack-demo-design-11-02/src/test/java/org/itstack/demo/design/test/ApiTest.java
@@ -11,6 +11,8 @@ import org.slf4j.LoggerFactory;
  * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
  * 公众号：bugstack虫洞栈
  * Create by 小傅哥(fustack) @2020
+ *享元模式
+ * 将变化与不变化的结合
  */
 public class ApiTest {
 
