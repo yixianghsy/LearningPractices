a713451eefd86228bfd74f450da3cdbf91d12861
yixianghsy <yixianghsy@gmail.com>
2023-04-05 09:50:19 +0800

    \

--- java/itstack-demo-design/itstack-demo-design-9-02/src/test/java/org/itstack/demo/design/test/ApiTest.java
+++ java/itstack-demo-design/itstack-demo-design-9-02/src/test/java/org/itstack/demo/design/test/ApiTest.java
@@ -4,6 +4,12 @@ import org.itstack.demo.design.LoginSsoDecorator;
 import org.itstack.demo.design.SsoInterceptor;
 import org.junit.Test;
 
+/**
+ * 装饰器的核⼼就是再不改原有类的基础上 )
+ * 给类新增功能。不改变原有类，可能有的⼩伙伴会想到继承、AOP切⾯，当然这些⽅式都可以实现，但
+ * 是使⽤装饰器模式会是另外⼀种思路更为灵活，可以避免继承导致的⼦类过多，也可以避免AOP带来的
+ * 复杂性
+ */
 public class ApiTest {
 
     @Test
