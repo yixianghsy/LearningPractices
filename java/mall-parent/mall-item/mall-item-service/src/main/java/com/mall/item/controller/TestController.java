ea6f3ca6df6bf0b915492c5c05f63a274583743e
yixianghsy <yixianghsy@gmail.com>
2023-07-25 00:10:59 +0800

    new

--- java/mall-parent/mall-item/mall-item-service/src/main/java/com/mall/item/controller/TestController.java
+++ java/mall-parent/mall-item/mall-item-service/src/main/java/com/mall/item/controller/TestController.java
@@ -5,7 +5,6 @@ import com.mall.pojo.EasyUIDataGridResult;
 import com.mall.utils.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
