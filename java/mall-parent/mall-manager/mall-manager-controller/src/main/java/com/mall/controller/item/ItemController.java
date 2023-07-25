9bfe3e65f4b81208bb043d462deea2f255ec34c6
yixianghsy <yixianghsy@gmail.com>
2023-07-25 10:13:47 +0800

    add  模块

---
+++ java/mall-parent/mall-manager/mall-manager-controller/src/main/java/com/mall/controller/item/ItemController.java
@@ -0,0 +1,37 @@
+package com.mall.controller.item;
+
+import com.mall.item.service.ItemService;
+import com.mall.modules.Item.TbItem;
+import com.mall.modules.Item.TbItemDesc;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 商品详情页面展示Controller
+ * <p>Title: ItemController</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.cn</p>
+ * @version 1.0
+ */
+@Controller
+public class ItemController {
+    @Reference
+    private ItemService itemService;
+    @RequestMapping("/item/{itemId}")
+    public String showItemInfo(@PathVariable Long itemId, Model model) {
+        System.out.println("进来了");
+        //调用服务取商品基本信息
+        TbItem tbItem = itemService.getItemById(itemId);
+//        Item item = new Item(tbItem);
+        //取商品描述信息
+        TbItemDesc tbItemDesc = itemService.getItemDescById(itemId);
+        //把信息传递给页面
+        model.addAttribute("item",tbItem);
+        model.addAttribute("itemDesc",tbItemDesc);
+        //返回逻辑视图
+        return "item";
+    }
+}
---
+++ java/mall-parent/mall-order/mall-order-service/src/test/java/com/mall/order/MallOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
