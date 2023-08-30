31ee4d2c8a5bc6d4012c6be94861316e7d4fef9b
yixianghsy <yixianghsy@gmail.com>
2023-08-30 10:03:33 +0800

    mq

--- java/mall-parent/mall-item/mall-item-controller/src/main/java/com/mall/item/message/GeneratePageMessageReceiver.java
+++ java/mall-parent/mall-item/mall-item-controller/src/main/java/com/mall/item/message/GeneratePageMessageReceiver.java
@@ -1,25 +1,38 @@
 package com.mall.item.message;
 
 
+import com.mall.item.confg.DefaultConsumerConfig;
 import com.mall.item.service.ItemService;
 import com.mall.item.vo.Item;
 import com.mall.modules.Item.TbItem;
 import com.mall.modules.Item.TbItemDesc;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.apache.dubbo.config.annotation.Reference;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 接收后台添加商品消息，生成商品详情页面
  */
 @Component
-public class GeneratePageMessageReceiver {
+public class GeneratePageMessageReceiver  extends DefaultConsumerConfig implements ApplicationListener<ContextRefreshedEvent> {
 
     @Reference
     private ItemService itemService;
@@ -29,27 +42,34 @@ public class GeneratePageMessageReceiver {
     private String TEMPLATE_NAME;
     @Value("${TEMPLATE_FILEPATH}")
     private String TEMPLATE_FILEPATH;
-    //待完成MQ
-//    @JmsListener(destination = "itemAddTopic", containerFactory = "jmsTopicListenerContainerFactory")
-    public void itemAddReceiver(Long itemId) {
-        try {
-            // 0、等待1s让e3-manager-service提交完事务，商品添加成功
-            Thread.sleep(1000);
-            // 1、准备商品数据
-            TbItem tbItem = itemService.getItemById(itemId);
-            TbItemDesc itemDesc = itemService.getItemDescById(itemId);
-            Item item = new Item(tbItem);
-            // 2、构造上下文(Model)
-            Context context = new Context();
-            context.setVariable("item", item);
-            context.setVariable("itemDesc", itemDesc);
-            // 3、生成页面
-            FileWriter writer = new FileWriter(TEMPLATE_FILEPATH + itemId + ".html");
-            springTemplateEngine.process(TEMPLATE_NAME, context, writer);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private static final Logger log = LoggerFactory.getLogger(GeneratePageMessageReceiver.class);
+    @Override
+    public ConsumeConcurrentlyStatus dealBody(List<MessageExt> msgs) {
+        msgs.forEach(msg -> {
+            byte[] bytes = msg.getBody();
+            try {
+                Long itemId = Long.valueOf(new String(bytes, "utf-8"));
+                log.info("新增id:",itemId);
+                TbItem tbItem = itemService.getItemById(itemId);
+                TbItemDesc itemDesc = itemService.getItemDescById(itemId);
+                Item item = new Item(tbItem);
+                // 2、构造上下文(Model)
+                Context context = new Context();
+                context.setVariable("item", item);
+                context.setVariable("itemDesc", itemDesc);
+                // 3、生成页面
+                FileWriter writer = new FileWriter(TEMPLATE_FILEPATH + itemId + ".html");
+                springTemplateEngine.process(TEMPLATE_NAME, context, writer);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+        return null;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+
     }
 }
