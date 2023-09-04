e4da3ef8890496352cb86912a03593c3b0182f22
yixianghsy <yixianghsy@gmail.com>
2023-09-04 12:24:45 +0800

    登录页面完成

---
+++ web/mall-admin/src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+/* Layout */
+import Layout from '../views/layout/Layout'
+export const constantRouterMap = [
+    {path: '/login', component: () => import('@/views/login/index'), hidden: true},
+    {path: '/404', component: () => import('@/views/404'), hidden: true},
+    {
+      path: '',
+      component: Layout,
+      redirect: '/home',
+      children: [{
+        path: 'home',
+        name: 'home',
+        component: () => import('@/views/home/index'),
+        meta: {title: '首页', icon: 'home'}
+      }]
+    }
+  ]
+  export const asyncRouterMap = [
+    {path: '*', redirect: '/404', hidden: true}
+  ]
+  export default new Router({
+    // mode: 'history', //后端支持可开
+    scrollBehavior: () => ({y: 0}),
+    routes: constantRouterMap
+  })
