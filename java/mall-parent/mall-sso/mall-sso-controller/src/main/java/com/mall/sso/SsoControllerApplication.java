7dc85d0830408ce541ec0c90617b24a27b567aad
yixianghsy <yixianghsy@gmail.com>
2023-10-13 01:06:02 +0800

    修复依赖问题

--- java/mall-parent/mall-sso/mall-sso-controller/src/main/java/com/mall/sso/SsoControllerApplication.java
+++ java/mall-parent/mall-sso/mall-sso-controller/src/main/java/com/mall/sso/SsoControllerApplication.java
@@ -4,14 +4,9 @@ import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-
 @EnableDubboConfig
 @DubboComponentScan("com.mall.*.service")
-/**
- * 目前推测可能是分页依赖中的数据源《com.github.pagehelper》
- */
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class SsoControllerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SsoControllerApplication.class, args);
