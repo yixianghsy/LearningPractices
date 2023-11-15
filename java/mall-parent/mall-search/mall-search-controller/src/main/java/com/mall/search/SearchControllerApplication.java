9a9aa6ddfd44935e55d1631a909361b78abbee5e
yixianghsy <yixianghsy@gmail.com>
2023-11-15 09:54:06 +0800

    修改前备份

--- java/mall-parent/mall-search/mall-search-controller/src/main/java/com/mall/search/SearchControllerApplication.java
+++ java/mall-parent/mall-search/mall-search-controller/src/main/java/com/mall/search/SearchControllerApplication.java
@@ -4,13 +4,15 @@ import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubboConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @EnableDubboConfig
 @DubboComponentScan("com.mall.*.service")
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SearchControllerApplication {
 
     public static void main(String[] args) {
---
+++ java/mall-parent/mall-search/mall-search-controller/src/main/java/com/mall/search/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.mall.search.config;
+
+
+import com.mall.config.BaseSwaggerConfig;
+import com.mall.domain.SwaggerProperties;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger相关配置
+ * Created by macro on 2018/4/26.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends BaseSwaggerConfig {
+
+    @Override
+    public SwaggerProperties swaggerProperties() {
+        return SwaggerProperties.builder()
+                .apiBasePackage("com.mall.mansger.service")
+                .title("mall后台系统")
+                .description("mall后台相关接口文档")
+                .contactName("macro")
+                .version("1.0")
+                .enableSecurity(true)
+                .build();
+    }
+
+    @Bean
+    public BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return generateBeanPostProcessor();
+    }
+
+}
